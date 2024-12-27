66b1c7ae850482e5206ada2a4622edce070a5cbf
FourNN <FlynnKUN@outlook.com>
2024-12-27 19:41:34 +0800

    配置SDL库

--- BioKun.cpp
+++ BioKun.cpp
@@ -1,9 +1,14 @@
-
-
-
+#define SDL_MAIN_HANDLED
+#include <SDL_image.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 
 int main(int argc, char* argv[])
 {
-    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    IMG_Init(IMG_INIT_PNG | IMG_INIT_PNG);
+    TTF_Init();
+    Mix_Init(MIX_INIT_MP3);
     return 0;
 }
